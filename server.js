c497c252ca3051847cf64725326d94185bb506b2
alirezarezairad <alirezarezairad5@gmail.com>
2024-11-18 16:05:19 +0100

    neue-update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import 'dotenv/config';
 const app = express();
+app.use(express.json());
 const PORT = process.env.SECRET_EXAMPLE_PORT || 3000;
 const KEY =process.env.SECRET_EXAMPLE_KEY;
 const MONGODB_URL =process.env.MONGODB_URL;
