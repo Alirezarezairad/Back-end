4fdd92a7a66d90c5b58dd40294a6319b810b54c4
alirezarezairad <alirezarezairad5@gmail.com>
2024-11-18 14:40:40 +0100

    3

--- server.js
+++ server.js
@@ -6,14 +6,35 @@ const PORT = process.env.SECRET_EXAMPLE_PORT || 3000;
 const KEY =process.env.SECRET_EXAMPLE_KEY;
 const MONGODB_URL =process.env.MONGODB_URL;
 const DB_NAME =process.env.DATABASE_NAME;
+//mit mongodb verbinden
 const client =new MongoClient(MONGODB_URL);
-const db = client.db(DB_NAME)
-app.get("/", (req, res) => {
-    res.send(`Hallo von Backend-server. KEY:${KEY}`);
-    console.log(req.url,req.method);
+
+const db = client.db(DB_NAME);
+
+app.get("/", async (req, res) => {
+  try{
+const docs = await db.collection("exampleCollection").find().toArray();
+res.send(docs);
+}catch(error){
+  console.error(error)
+  res.status(500)
+}
   });
-  
+
+  app.post("/createDoc", async (req,res)=> {
+   const content =req.body;
+   try {
+    const result = await db.collection("exampleColletion").insertOne(content);
+    console.log(result);
+    res.status(201).send(result)
+   }catch (error){
+    console.error(error);
+    res.status(500)
+   }
+
+  });
+
   app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
+    console.log(`Server läuft auf port:${PORT}`);
   });
   
