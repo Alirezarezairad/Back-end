f0413ce9502ae603f471200fa99da42040fcd830
alirezarezairad <alirezarezairad5@gmail.com>
2024-11-18 11:14:37 +0100

    update

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
+const app = express();
+const PORT = process.env.SECRET_EXAMPLE_PORT || 3000;
+const KEY =process.env.SECRET_EXAMPLE_KEY;
+const MONGODB_URL =process.env.MONGODB_URL;
+const DB_NAME =process.env.DATABASE_NAME:
+const client =new MongoClient(MONGODB_URL);
+const db = client.db(DB_NAME)
+app.get("/", (req, res) => {
+    res.send(`Hallo von Backend-server. KEY:${KEY}`);
+    console.log(req.url,req.method);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+  });
+  
